543292857ae1e7bee960ae59b53ba7f81de6b705
hamza19944 <hamza.elmansy94@gmail.com>
2022-12-06 00:13:45 +0300

    adding the variables

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,16 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_HOST=database-1.c3rlmvpatfhe.us-east-1.rds.amazonaws.com
+export DB_PORT=5432
+export PORT=8080
+export POSTGRES_PASSWORD=postgres
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export RDS_DIALECT=postgres
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export AWS_BUCKET=arn:aws:s3:::hamza-udagram
+export URL=http://udagram-api-dev.eba-iykrxarb.us-east-1.elasticbeanstalk.com/
+export AWS_ACCESS_KEY_ID=AKIAU7J3D37FNWCZXNTJ
+export AWS_SECRET_ACCESS_KEY=HCn9FqNS0oG4f9LHSKLuc0jvL10SwtnVNd3ziBot
+export JWT_SECRET=yoursecretinawell
